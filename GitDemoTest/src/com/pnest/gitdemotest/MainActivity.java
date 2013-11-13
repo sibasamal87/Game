9dddc52b7ca8f3efcc80e579d4e4fd5f1ef9ad78
kodc.siba <kodc.siba@gmail.com>
2013-11-13 17:15:07 +0530

    data updated

--- GitDemoTest/src/com/pnest/gitdemotest/MainActivity.java
+++ GitDemoTest/src/com/pnest/gitdemotest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("inside test of git 3");
+		System.out.println("inside test of git 4");
 	}
 
 	@Override
