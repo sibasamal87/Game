c46e458f12b5ef5ecd77c44523e180e1383427c9
kodc.siba <kodc.siba@gmail.com>
2013-11-13 17:51:16 +0530

    commited

--- GitDemoTest/src/com/pnest/gitdemotest/MainActivity.java
+++ GitDemoTest/src/com/pnest/gitdemotest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("inside test of git 6");
+		System.out.println("inside test of git 7");
 	}
 
 	@Override
